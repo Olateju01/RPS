34d8bd54180091ef6ad34b623adf1c4ad1d41935
Olateju <3legged01@gmail.com>
2023-11-22 19:21:08 +0000

    implemented enum class in rps.py

--- rps.py
+++ rps.py
@@ -1,15 +1,24 @@
 import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
 
 player_choice = input(" Enter...\n 1 for Rock \n 2 for Paper or \n 3 for Scissors\n")
 player = int(player_choice)
 if player < 1 or player > 3:
     print(f"You can only enter 1,2 or 3")
-else:
-    print(f"You chose {player}")
+    
 
 computer_choice = random.choice("123")
 computer = int(computer_choice)
-print(f"computer chose {computer}")
+nplayer = str(RPS(player)).replace("RPS.","")
+cplayer = str(RPS(computer)).replace("RPS.","")
+print(f"You chose {nplayer}")
+print(f"computer chose {cplayer}")
 
 if player == 1 and computer== 3:
     print(f"You win!🥳")
